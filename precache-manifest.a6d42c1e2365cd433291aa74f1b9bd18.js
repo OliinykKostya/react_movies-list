1c1228378bc63a15ac1d0b1dfbf86c96179bdd48
Kostya <oliinyk.kostya@gmail.com>
2020-04-23 20:16:46 +0300

    Updates

---
+++ precache-manifest.a6d42c1e2365cd433291aa74f1b9bd18.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cadd2cf86ecf054f1e0d5ff0455dda5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "227b1dd5e868986bab93",
+    "url": "./static/css/2.cd1899df.chunk.css"
+  },
+  {
+    "revision": "a5cb8ef875a84de3eea1",
+    "url": "./static/css/main.8a2ebc01.chunk.css"
+  },
+  {
+    "revision": "227b1dd5e868986bab93",
+    "url": "./static/js/2.2b83b950.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2b83b950.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5cb8ef875a84de3eea1",
+    "url": "./static/js/main.ba1527c4.chunk.js"
+  },
+  {
+    "revision": "383414d4f5edafe01bf9",
+    "url": "./static/js/runtime-main.e9d4b854.js"
+  }
+]);
